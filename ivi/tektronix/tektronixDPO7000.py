c47d69d640180d0e56a47138da3990a335e76ef7
Nick Edwards <nedwards@gradientone.com>
2016-12-05 12:42:28 -0800

    New DPO features to support GRL

--- ivi/tektronix/tektronixDPO7000.py
+++ ivi/tektronix/tektronixDPO7000.py
@@ -43,3 +43,107 @@ class tektronixDPO7000(tektronixBaseScope):
         self._identity_supported_instrument_models = ['DPO7354C']
 
         self._init_channels()
+
+        self._add_property('acquisition.horizontal_mode',
+                        self._get_acquisition_horizontal_mode,
+                        self._set_acquisition_horizontal_mode,
+                        None,
+                        ivi.Doc("""
+                        This command set or queries the horizontal mode. Auto mode is the factory
+                        default. There are three horizontal modes:
+                        * 'auto': AUTO selects the automatic horizontal model. Auto mode attempts to keep record
+                        length constant as you change the time per division setting.  Record length is
+                        read only.
+                        * 'constant' selects the constant horizontal model. Constant mode attempts to keep
+                        sample rate constant as you change the time per division setting. Record length
+                        is read only.
+                        * 'manual' selects the manual horizontal model. Manual mode lets you change
+                        sample mode and record length. Time per division or Horizontal scale is read only.
+                        """))
+
+        self._add_property('acquisition.sample_rate',
+                        self._get_acquisition_sample_rate,
+                        self._set_acquisition_sample_rate,
+                        None,
+                        None,
+                        ivi.Doc("""
+                        Returns the effective sample rate of the acquired waveform using the
+                        current configuration. The units are samples per second.
+                        """, cls, grp, '4.2.10'))
+
+        self._add_property('acquisition.horizontal_roll',
+                        self._get_acquisition_horizontal_roll,
+                        self._set_acquisition_horizontal_roll,
+                        None,
+                        ivi.Doc("""
+                        This command sets or queries the Roll Mode status. Use Roll Mode when you
+                        want to view data at very slow sweep speeds. It is useful for observing data
+                        samples on the screen as they occur.  There are three modes:
+                        * 'auto': AUTO enables Roll Mode, if the time/division is set appropriately.
+                        * 'off': OFF disables Roll Mode.
+                        * 'on': ON enables Roll Mode, if the time/division is set approprately.
+                        """))
+
+    def _get_acquisition_horizontal_mode(self):
+        if not self._driver_operation_simulate and not self._get_cache_valid():
+            self._acquisition_horizontal_mode = self._ask(":horizontal:mode?")
+            self._set_cache_valid()
+        return self._aquisition_horizontal_mode
+
+    def _set_acquisition_horizontal_mode(self, value):
+        if not self._driver_operation_simulate:
+            self._write(":horizontal:mode %e" % value)
+        self._acquisition_horizontal_mode = value
+        self._set_cache_valid()
+
+    def _get_acquisition_sample_rate(self):
+        return self._get_acquisition_record_length() / self._get_acquisition_time_per_record()
+
+    def _set_acquisition_sample_rate(self, value):
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":horizontal:mode:samplerate %e" % value)
+        self._acquisition_sample_rate = value
+        self._set_cache_valid()
+
+    def _get_acquisition_horizontal_roll(self):
+        if not self._driver_operation_simulate and not self._get_cache_valid():
+            self._horizontal_roll = self._ask(":horizontal:roll?")
+            self._set_cache_valid()
+        return self._acquisition_horizontal_roll
+
+    def _set_acquisition_horizontal_roll(self, value):
+        if not self._driver_operation_simulate:
+            self._write(":horizontal:roll %e" % value)
+        self._acquisition_horizontal_roll = value
+        self._set_cache_valid()
+
+    def _get_timebase_scale(self):
+        if not self._driver_operation_simulate and not self._get_cache_valid():
+            self._timebase_scale = float(self._ask(":horizontal:mode:scale?"))
+            self._timebase_range = self._timebase_scale * self._horizontal_divisions
+            self._set_cache_valid()
+        return self._timebase_scale
+
+    def _set_timebase_scale(self, value):
+        value = float(value)
+        if not self._driver_operation_simulate:
+            self._write(":horizontal:mode:scale %e" % value)
+        self._timebase_scale = value
+        self._timebase_range = value * self._horizontal_divisions
+        self._set_cache_valid()
+        self._set_cache_valid(False, 'timebase_window_range')
+
+    def _get_acquisition_number_of_points_minimum(self):
+        if not self._driver_operation_simulate and not self._get_cache_valid():
+            self._acquisition_number_of_points_minimum = int(self._ask(":horizontal:mode:recordlength?"))
+            self._set_cache_valid()
+        return self._acquisition_number_of_points_minimum
+
+    def _set_acquisition_number_of_points_minimum(self, value):
+        value = int(value)
+        # coerce value?
+        if not self._driver_operation_simulate:
+            self._write(":horizontal:mode:recordlength %d" % value)
+        self._acquisition_number_of_points_minimum = value
+        self._set_cache_valid()
