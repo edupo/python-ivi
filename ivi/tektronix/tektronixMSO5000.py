7fea817dc99797d2074bd8103f616ff072ab7f25
Nick Edwards <nedwards@gradientone.com>
2017-02-24 13:22:39 -0800

    Updated measurement mapping to support negative and positive overshoot

--- ivi/tektronix/tektronixMSO5000.py
+++ ivi/tektronix/tektronixMSO5000.py
@@ -26,6 +26,49 @@ THE SOFTWARE.
 
 from .tektronixDPO5000 import *
 
+MeasurementFunctionMapping = {
+        'rise_time': 'rise',
+        'fall_time': 'fall',
+        'frequency': 'frequency',
+        'period': 'period',
+        'voltage_rms': 'rms',
+        'voltage_peak_to_peak': 'pk2pk',
+        'voltage_max': 'maximum',
+        'voltage_min': 'minimum',
+        'voltage_high': 'high',
+        'voltage_low': 'low',
+        'voltage_average': 'mean',
+        'width_negative': 'nwidth',
+        'width_positive': 'pwidth',
+        'duty_cycle_negative': 'nduty',
+        'duty_cycle_positive': 'pduty',
+        'amplitude': 'amplitude',
+        'voltage_cycle_rms': 'crms',
+        'voltage_cycle_average': 'cmean',
+        'overshoot': 'tovershoot',
+
+        'area': 'area',
+        'burst': 'burst',
+        'cycle_area': 'carea',
+        'overshoot_negative': 'novershoot',
+        'overshoot_positive': 'povershoot',
+        'edgecount_negative': 'nedgecount',
+        'edgecount_positive': 'pedgecount',
+        'pulsecount_negative': 'npulsecount',
+        'pulsecount_positive': 'ppulsecount',
+
+        'histogram_hits': 'hits',
+        'histogram_peak_hits': 'peakhits',
+        'histogram_median': 'median',
+        'histogram_sigma1': 'sigma1',
+        'histogram_sigma2': 'sigma2',
+        'histogram_sigma3': 'sigma3',
+        'histogram_stdev': 'stdev',
+        'histogram_waveforms': 'waveforms',
+
+        'phase': 'phase',
+        'delay': 'delay'}
+
 class tektronixMSO5000(tektronixDPO5000):
     "Tektronix MSO4000 series IVI oscilloscope driver"
 
