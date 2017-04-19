87a6458f80f03fe94f1c09e1223eac34bbdd046f
Nick Edwards <nedwards@gradientone.com>
2017-04-19 09:07:15 -0700

    First test checkin of new rigol

--- ivi/rigol/__init__.py
+++ ivi/rigol/__init__.py
@@ -2,7 +2,7 @@
 
 Python Interchangeable Virtual Instrument Library
 
-Copyright (c) 2013-2016 Alex Forencich
+Copyright (c) 2013-2017 Alex Forencich
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,39 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 """
+
+# Oscilloscopes
+# DS1000Z
+from .rigolDS1074Z import rigolDS1074Z
+from .rigolDS1104Z import rigolDS1104Z
+from .rigolMSO1074Z import rigolMSO1074Z
+from .rigolMSO1104Z import rigolMSO1104Z
+# DS2000A
+from .rigolDS2072A import rigolDS2072A
+from .rigolDS2102A import rigolDS2102A
+from .rigolDS2202A import rigolDS2202A
+from .rigolDS2302A import rigolDS2302A
+from .rigolMSO2072A import rigolMSO2072A
+from .rigolMSO2102A import rigolMSO2102A
+from .rigolMSO2202A import rigolMSO2202A
+from .rigolMSO2302A import rigolMSO2302A
+# DS4000
+from .rigolDS4012 import rigolDS4012
+from .rigolDS4014 import rigolDS4014
+from .rigolDS4022 import rigolDS4022
+from .rigolDS4024 import rigolDS4024
+from .rigolDS4032 import rigolDS4032
+from .rigolDS4034 import rigolDS4034
+from .rigolDS4052 import rigolDS4052
+from .rigolDS4054 import rigolDS4054
+from .rigolMSO4012 import rigolMSO4012
+from .rigolMSO4014 import rigolMSO4014
+from .rigolMSO4022 import rigolMSO4022
+from .rigolMSO4024 import rigolMSO4024
+from .rigolMSO4032 import rigolMSO4032
+from .rigolMSO4034 import rigolMSO4034
+from .rigolMSO4052 import rigolMSO4052
+from .rigolMSO4054 import rigolMSO4054
 
 # DC Power Supplies
 # DP800
